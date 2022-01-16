43867398d789df4399755c0b096845bf517836fa
ENESABD <72234612+ENESABD@users.noreply.github.com>
2022-01-16 02:06:54 -0500

    For deployment

--- server/index.js
+++ server/index.js
@@ -134,7 +134,7 @@ app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
  });
 
-app.listen(process.env.PORT || 8000, () => console.log(`listening ${PORT}`))
+app.listen(process.env.PORT || 8000, () => console.log(`listening`))
 
 
 
