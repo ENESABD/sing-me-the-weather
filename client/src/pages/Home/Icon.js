9b6b5bfd295be89d976b49582619630fe7a7fc7d
Mahmud Enes <eabdulla@wesleyan.edu>
2022-07-26 05:08:40 -0400

    Fixed an issue with the icons not showing

--- client/src/pages/Home/Icon.js
+++ client/src/pages/Home/Icon.js
@@ -2,10 +2,11 @@ import React, { Fragment } from 'react';
 
 function Icon({ iconCode }) {
 
+
     return (
         <Fragment>
             {iconCode ? 
-                <img src={require(`../../assets/icons/${iconCode}.svg`).default} alt="weather icon" width="50" height="50" /> 
+                <img src={require(`../../assets/icons/${iconCode}.svg`)} alt="weather icon" width="50" height="50" /> 
             : null}
         </Fragment>
             
